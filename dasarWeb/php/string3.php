d8fc762ff052774dedf9fccfaa96c31eb257845d
netyst <pineapplekuu@gmail.com>
2025-10-02 14:25:30 +0700

    membalikk

--- dasarWeb/php/string3.php
+++ dasarWeb/php/string3.php
@@ -1,4 +1,12 @@
 <?php
-$pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
+
+$pesan = "saya arek malang";
+
+$pesanPerKata = explode(" ", $pesan);
+
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 ?>
